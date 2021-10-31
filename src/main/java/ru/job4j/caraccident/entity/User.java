4e0285cf322a35fa0cd74f7dae33727ddf2745c5
romankhiropulos <romankhiropulos@gmail.com>
2021-10-31 22:34:38 +0300

    Prepare repo Caraccident2

--- src/main/java/ru/job4j/caraccident/entity/User.java
+++ src/main/java/ru/job4j/caraccident/entity/User.java
@@ -1,4 +1,15 @@
 package ru.job4j.caraccident.entity;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    public User() {
+
+    }
 }
