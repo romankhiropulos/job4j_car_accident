17339eab2c79698e67e23eaae591e7347ab331aa
romankhiropulos <romankhiropulos@gmail.com>
2021-11-01 19:12:05 +0300

    maven-war-plugin fix

--- src/main/java/ru/job4j/caraccident/entity/User.java
+++ src/main/java/ru/job4j/caraccident/entity/User.java
@@ -1,6 +1,7 @@
 package ru.job4j.caraccident.entity;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 public class User {
@@ -11,5 +12,30 @@ public class User {
 
     public User() {
 
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
