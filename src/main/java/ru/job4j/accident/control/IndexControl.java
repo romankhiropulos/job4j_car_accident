2047e68e9a2ba11a57f8a13b5097a717ac98e18e
romankhiropulos <romankhiropulos@gmail.com>
2021-11-05 22:07:46 +0300

    Write list of string on index.jsp

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -4,10 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class IndexControl {
     @GetMapping("/")
     public String index(Model model) {
+        List<String> accidents  = new ArrayList<>(Arrays.asList("Авария", "Столкновение", "Обгон"));
+        model.addAttribute("accidents", accidents);
         return "index";
     }
 }
